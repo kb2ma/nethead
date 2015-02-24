934f9768fa2209586a4ed252fa1e44fd26aa3cc4
Ken Bannister <kb2ma@runbox.com>
2015-02-24 12:53:49 -0500

    Issue 2. Add Nagios service record for new RSS neighbor.
    * Insert service record in Nagios config file for the host. Adagios uses
      this approach when adding a service via the UI.
    * Also, always use 'mote-<last-4-addr-chars>.cfg' for the name of the
      Nagios config file for host.

--- nethead/manager.py
+++ nethead/manager.py
@@ -25,6 +25,15 @@ from   soscoap import CodeClass
 from   soscoap import ServerResponseCode
 from   soscoap import SuccessResponseCode
 from   soscoap.server import CoapServer
+        
+def getInvariantName(host):
+    '''Provides a short string to name a host, based on host attributes that don't
+    change.
+    
+    :param Host host: Adagios host
+    :return:
+    '''
+    return 'mote-{0}'.format(host.address[-4:])
 
 class HostManager(object):
     '''
@@ -42,7 +51,8 @@ class HostManager(object):
         log.debug('GET resource path is {0}'.format(resource.path))
     
     def _postResource(self, resource):
-        '''Records the value from the provided resource, from a POST request.
+        '''Records the value from the provided resource, from a POST request. 
+        Creates Nagios host and service records if not found.
         
         Assumes the CoAP server handles any raised exception.
         
@@ -71,19 +81,30 @@ class HostManager(object):
             host = hosts[0]
             
             for (nbrAddr,intValue) in resource.value.items():
-                # Search for existing service record for this neighbor
+                # Search for existing service record for this neighbor; create if none
                 serviceName = 'rss-{0}'.format(nbrAddr)
-                services    = Service.objects.filter(service_description=serviceName)
+                services    = Service.objects.filter(host_name=host.host_name, 
+                                                     service_description=serviceName)
+                service     = None
                 if not services:
-                    # Service creation example: https://github.com/pynag/pynag/wiki/Model
-                    log.warn('/nh/rss: Service not found: {0}'.format(serviceName))
-                    resource.resultClass = CodeClass.ClientError
-                    resource.resultCode  = ClientResponseCode.PreconditionFailed
-                    continue
-                elif len(services) > 1:
-                    log.warn('Found {0} services; using the first one'.format(len(services)))
-                    
-                service = services[0]
+                    service = self._createRssService(host, nbrAddr)
+                    if service:
+                        log.info('/nh/rss: Created service {0} for host {1}'.format(service.service_description, 
+                                                                                    service.host_name))
+                        resource.resultClass = CodeClass.Success
+                        resource.resultCode  = SuccessResponseCode.Created
+                    else:
+                        log.error('/nh/rss: Service creation failed for rss-{0} for host {1}'.format(nbrAddr, 
+                                                                                                     host.host_name))
+                        # How to notify that a *particular* service creation failed?
+                        # Tally success and failure, and send some text back?
+                        #resource.resultClass = CodeClass.ServerError
+                        #resource.resultCode  = ServerResponseCode.InternalServerError
+                        continue
+                else:
+                    if len(services) > 1:
+                        log.warn('Found {0} services; using the first one'.format(len(services)))
+                    service = services[0]
                 
                 # Submit RSS
                 pynag.Utils.send_nsca(pynag.Plugins.OK, 
@@ -116,7 +137,7 @@ class HostManager(object):
                 resource.resultCode  = ClientResponseCode.BadRequest
                 return
             
-            # Search for existing host record
+            # Search for existing host record; create if none
             hosts = Host.objects.filter(address=resource.sourceAddress[0])
             if not hosts:
                 host = self._createHost(resource.sourceAddress[0])
@@ -126,8 +147,8 @@ class HostManager(object):
                     resource.resultCode  = SuccessResponseCode.Created
                 else:
                     log.error('/nh/lo: Host creation failed for {0}'.format(resource.sourceAddress[0]))
-                    msg.codeClass   = CodeClass.ServerError
-                    msg.codeDetail  = ServerResponseCode.InternalServerError
+                    resource.resultClass = CodeClass.ServerError
+                    resource.resultCode  = ServerResponseCode.InternalServerError
             else:
                 if len(hosts) > 1:
                     log.warn('Found {0} hosts; using the first one'.format(len(hosts)))
@@ -147,19 +168,44 @@ class HostManager(object):
         '''
         host = Host()
         
-        host.host_name              = 'mote-{0}'.format(ipAddress[-4:])
         host.address                = ipAddress
+        host.host_name              = getInvariantName(host)    # requires ipAddress
         host.active_checks_enabled  = 0
-        host.check_interval         = 5     # must define these two values,
-        host.max_check_attempts     = 5     # even though not used
+        host.check_interval         = 5                         # must define these two values,
+        host.max_check_attempts     = 5                         # even though not used
         host.passive_checks_enabled = 1
         
         host.set_filename('/etc/nagios3/okconfig/hosts/{0}.cfg'.format(host.host_name))
         host.save()
         return host
 
-    def _createHelloPath(self):
-        '''Creates a random 4 hex-char path to the registration data for a host
+    def _createRssService(self, host, nbrAddr):
+        '''Creates a Nagios service entry for RSS readings for transmissiosn received 
+        by a host from a particular neighbor. Saves the service configuration 
+        in the file for the host, because Adagios uses this organization when
+        adding a service in the UI.
+        
+        :param Host host: Adagios host
+        :param str nbrAddr: Last four hex chars in neighbor address
+        :return: The created service
+        :rtype: Service
+        '''
+        service = Service()
+        
+        service.host_name              = host.host_name
+        service.service_description    = 'rss-{0}'.format(nbrAddr)
+        service.active_checks_enabled  = 0
+        service.check_command          = 'return-ok'
+        service.check_interval         = 1     # must define these two values,
+        service.max_check_attempts     = 5     # even though not used
+        service.passive_checks_enabled = 1
+        
+        service.set_filename('/etc/nagios3/okconfig/hosts/{0}.cfg'.format(getInvariantName(host)))
+        service.save()
+        return service
+
+    def _genHelloPath(self):
+        '''Generates a random 4 hex-char path to the registration data for a host.
         
         2014-12-30 Not used at present; retain as a template for any kind of token 
         generation.
